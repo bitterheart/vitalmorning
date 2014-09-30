22efc95772730e27f68f9e5ec83873f9b4b21b7e
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-09-30 23:30:11 +0000

    now trying to read value

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -2,11 +2,9 @@
     "use strict";
     app.controller("controller",["$scope","LocalStorageService",function($scope,localStorageService){
         $scope.name="xxx";
-        /*
         localStorageService.getName().then(function(name){
             $scope.name=name;
         });
-        */
         $scope.saveName=function(){
             localStorageService.setName($scope.name);
         };
