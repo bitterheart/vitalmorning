372b992a7587bf69fe159c8f3cedfb28f0f763d7
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-09-30 23:18:16 +0000

    adding local storage

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -2,9 +2,11 @@
     "use strict";
     app.controller('controller','LocalStorageService',["$scope",function($scope,localStorageService){
         $scope.name='xxx';
+        /*
         localStorageService.getName().then(function(name){
             $scope.name=name;
         });
+        */
         $scope.saveName=function(){
             localStorageService.setName($scope.name);
         };
