57e570dfb38eceda5a5d800447096e66f4fd5b7c
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-09-30 23:16:24 +0000

    adding local storage

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -1,6 +1,12 @@
 (function(app){
     "use strict";
-    app.controller('controller',["$scope",function($scope){
-        $scope.name='emory';
+    app.controller('controller','LocalStorageService',["$scope",function($scope,localStorageService){
+        $scope.name='xxx';
+        localStorageService.getName().then(function(name){
+            $scope.name=name;
+        });
+        $scope.saveName=function(){
+            localStorageService.setName($scope.name);
+        };
     }]);
 }(window.angular.module('app')));
---
+++ public/scripts/services.js
@@ -0,0 +1,16 @@
+(function(app){
+    "use strict";
+    app.service('LocalStorageService',["$q",function($q){
+        this.setName=function(name){
+            var deferred=$q.defer();
+            localStorage.setItem('name',name);
+            deferred.fulfill();
+            return deferred.promise;
+        };
+        this.getName=function(){
+            var deferred=$q.defer();
+            deferred.resolve(localStorage.getItem('name'));
+            return deferred.promise;
+        }
+    }]);
+}(window.angular.module('app')));
