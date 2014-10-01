8ba3788473fb246e6f7711665204af24307d2224
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-10-01 13:10:08 +0000

    added jenkins branch

---
+++ bin/e2e-tests.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+sudo yum update -y
+sudo yum install -y wget
+if [ ! -f epel-release-7-1.noarch.rpm ]
+then
+    wget http://mirror.sfo12.us.leaseweb.net/epel/7/x86_64/e/epel-release-7-1.noarch.rpm
+    sudo yum install -y epel-release-7-1.noarch.rpm || true
+fi
+sudo yum update -y
+sudo yum -y install nodejs-grunt-cli npm Xvfb ImageMagick bzip2
+if [ ! -f google-chrome-stable_current_x86_64.rpm ]
+then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+    sudo yum install -y google-chrome-stable_current_x86_64.rpm
+fi
+sudo yum update -y
+npm install
+if [ ! -d /tmp/.X99-lock ]
+then
+    Xvfb :99 -ac -screen 0 1024x768x24 &
+    export DISPLAY=:99
+fi
+./node_modules/protractor/bin/webdriver-manager update --standalone  
+grunt e2etests-ci
