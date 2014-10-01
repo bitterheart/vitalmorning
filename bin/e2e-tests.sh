961c7ffc85fc4efd062cb2908d2ac2bdcbf2eb10
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-10-01 13:50:47 +0000

    use 7.2

--- bin/e2e-tests.sh
+++ bin/e2e-tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 sudo yum update -y
-# if [ ! -f epel-release-7-1.noarch.rpm ]
-# then
-    wget http://mirror.sfo12.us.leaseweb.net/epel/7/x86_64/e/epel-release-7-1.noarch.rpm
-    sudo yum install -y epel-release-7-1.noarch.rpm || true
+if [ ! -f epel-release-7-2.noarch.rpm ]
+then
+    wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+    sudo yum install -y epel-release-7-2.noarch.rpm
     sudo yum update -y
-# fi
+fi
 sudo yum -y install nodejs-grunt-cli npm Xvfb ImageMagick bzip2
 if [ ! -f google-chrome-stable_current_x86_64.rpm ]
 then
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
     sudo yum install -y google-chrome-stable_current_x86_64.rpm
+    sudo yum update -y
 fi
-sudo yum update -y
 npm install
 if [ ! -f /tmp/.X99-lock ]
 then
@@ -21,9 +21,3 @@ then
 fi
 ./node_modules/protractor/bin/webdriver-manager update --standalone  
 grunt e2etests-ci
-echo aaaaaaaaaaaaaaaaaaaaaa
-yum provides *bin/npm
-echo aaaaaaaaaaaaaaaaaaaaaa
-yum provides *bin/grunt
-echo aaaaaaaaaaaaaaaaaaaaaa
-sudo yum install -y wget
