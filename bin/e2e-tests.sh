b696a0bf041afacd67efe1fbe20b44abaf4a2cf2
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-10-01 13:46:14 +0000

    let us force epell

--- bin/e2e-tests.sh
+++ bin/e2e-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo yum update -y
-if [ ! -f epel-release-7-1.noarch.rpm ]
-then
+# if [ ! -f epel-release-7-1.noarch.rpm ]
+# then
     wget http://mirror.sfo12.us.leaseweb.net/epel/7/x86_64/e/epel-release-7-1.noarch.rpm
     sudo yum install -y epel-release-7-1.noarch.rpm || true
-fi
-sudo yum update -y
+    sudo yum update -y
+# fi
 sudo yum -y install nodejs-grunt-cli npm Xvfb ImageMagick bzip2
 if [ ! -f google-chrome-stable_current_x86_64.rpm ]
 then
