3ae2e3ade66467b0d35b85f6c7399a3558a56001
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-10-01 19:27:41 +0000

    fixxing scripting error

--- bin/unit-tests.sh
+++ bin/unit-tests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 sudo yum update -y
+if [ ! -f epel-release-7-2.noarch.rpm ]
+then
+    wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+    sudo yum install -y epel-release-7-2.noarch.rpm
+    sudo yum update -y
+fi
 sudo yum -y install nodejs-grunt-cli npm bzip2
 npm install
 grunt unit-tests-ci
