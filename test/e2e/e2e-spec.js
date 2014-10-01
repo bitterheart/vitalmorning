549f0918419516b58f7d1659ef783b31b8599eda
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-10-01 15:56:43 +0000

    worked on tests, there is an error in the tests (on purpose)

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -1,8 +1,49 @@
 describe('simple tests', function() {
-    beforeEach(function(){
-       browser.get('index.html');
+    describe('a very simple test', function() {
+        beforeEach(function() {
+            browser.get('index.html');
+        });
+        it('fails', function() {
+            expect(element(by.css('span')).getText()).toEqual('');
+        });
     });
-    it('fails', function() {
-        expect(element(by.css('span')).getText()).toEqual('');
+    describe('add and verify', function() {
+        beforeEach(function() {
+            browser.get('index.html');
+            element(by.css('span.deleteAll')).click();
+            element(by.css('span.addEntry')).click();
+            element(by.css('div.addition input.loginPage')).sendKeys('http://something.com');
+            element(by.css('div.addition input.userId')).sendKeys('userid');
+            element(by.css('div.addition input.password')).sendKeys('password');
+            element(by.css('div.addition span.add')).click();
+            browser.get('index.html');
+        });
+        it('verify', function() {
+            element.all(by.css('div.entries div.entry')).then(function(entries) {
+                expect(entries.length).toEqual(1);
+                expect(entries[0].element(by.css('span.loginPage')).getText()).toEqual('http://something.com');
+                expect(entries[0].element(by.css('span.userId')).getText()).toEqual('userid');
+                expect(entries[0].element(by.css('span.password')).getText()).toEqual('password');
+                expect(entries[0].element(by.css('span.loginPage')).getText()).toEqual('wrong');
+            });
+        });
+    });
+    describe('verify delete all',function(){
+        beforeEach(function() {
+            browser.get('index.html');
+            element(by.css('span.addEntry')).click();
+            element(by.css('div.addition input.loginPage')).sendKeys('http://something.com');
+            element(by.css('div.addition input.userId')).sendKeys('userid');
+            element(by.css('div.addition input.password')).sendKeys('password');
+            element(by.css('div.addition span.add')).click();
+            browser.get('index.html');
+        });
+        it('verify', function() {
+            element(by.css('span.deleteAll')).click();
+            element.all(by.css('div.entries div.entry')).then(function(entries) {
+                expect(entries.length).toEqual(0);
+                expect(entries.length).toEqual(1);
+            });
+        });
     });
 });
