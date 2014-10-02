48a97cc82b6a1d15d9705186f92d80b03ae174ed
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-10-02 02:30:32 +0000

    fixed templating

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -19,8 +19,8 @@ describe('simple tests', function() {
             browser.get('index.html');
         });
         it('verify', function() {
-            expect(element(by.css('div.entries div.entry:nth-child(1) span.loginPage')).getText()).toEqual('http://something.com');
                 browser.get('index.html');
+            expect(element(by.css('div.entries div.entry:nth-child(1) span.loginPage')).getText()).toEqual('http://something.com');
             element.all(by.repeater('entry in entries')).then(function(entries) {
                 expect(entries.length).toEqual(1);
                 expect(entries[0].element(by.css('span.loginPage')).getText()).toEqual('http://something.com');
