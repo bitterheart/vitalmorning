b432ab88008165147f7eb53f0a6f173bd3b349fb
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-10-02 19:48:52 +0000

    #43 crx

--- Gruntfile.js
+++ Gruntfile.js
@@ -126,8 +126,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-simple-crx');
   grunt.loadNpmTasks('grunt-chrome-compile');
   grunt.registerTask('e2e-tests-cli', ['express:cli', 'protractor_webdriver:cli', 'protractor:cli']);
-  grunt.registerTask('e2e-tests-ci', ['express:ci', 'protractor_webdriver:ci', 'protractor:ci']);
+  grunt.registerTask('e2e-tests-ci', ['chrome-extension','express:ci', 'protractor_webdriver:ci', 'protractor:ci']);
   grunt.registerTask('unit-tests-cli', ['karma:cli']);
   grunt.registerTask('unit-tests-ci', ['karma:ci']);
-  grunt.registerTask('crx', ['crx:ci'])
 };
