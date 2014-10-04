0799baf94a717019323b691cf6fd2a6da2aa79b6
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-10-04 02:14:47 +0000

    #62grunt bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,6 +123,10 @@ module.exports = function(grunt) {
         },
         target: ['Gruntfile.js','bower.json','manifest.json','package.json','public/scripts/*.json','config/*.js']
     },
+      bower: {
+    install: {
+    }
+  },
     bowerInstall: {
       target: {
         src: [
@@ -149,7 +153,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-bower-install');
   grunt.loadNpmTasks('grunt-eslint');
   grunt.registerTask('e2e-tests-cli', ['express:cli', 'protractor_webdriver:cli', 'protractor:cli']);
-  grunt.registerTask('e2e-tests-ci', ['bowerInstall','chrome-extension', 'express:ci', 'protractor_webdriver:ci', 'protractor:ci']);
+  grunt.registerTask('e2e-tests-ci', ['bower:install', 'bowerInstall','chrome-extension', 'express:ci', 'protractor_webdriver:ci', 'protractor:ci']);
   grunt.registerTask('unit-tests-cli', ['karma:cli']);
   grunt.registerTask('unit-tests-ci', ['karma:ci']);
 };
