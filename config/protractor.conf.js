d015bf62e09f65b16ef1b0f5e9799740fa47ac34
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-10-04 03:06:32 +0000

    #62grunt bower

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -1,45 +1,42 @@
-"use strict";
-module.exports.config = {
-  seleniumAddress: "http://localhost:4444/wd/hub",
-  specs: [
-    "../test/e2e/*.js"
-  ],
-  capabilities: {
-    browserName: "chrome",
-    chromeOptions: {
-      binary: "/usr/bin/google-chrome",
-      args: ["--disable-web-security"]
+module.exports.config = function() {
+  "use strict";
+  return {
+    seleniumAddress: "http://localhost:4444/wd/hub",
+    specs: [
+      "../test/e2e/*.js"
+    ],
+    capabilities: {
+      browserName: "chrome",
+      chromeOptions: {
+        binary: "/usr/bin/google-chrome",
+        args: ["--disable-web-security"]
+      },
+      count: 1,
+      shardTestFiles: false,
+      maxInstances: 1
     },
-    count: 1,
-    shardTestFiles: false,
-    maxInstances: 1
-  },
-  multiCapabilities: [],
-  maxSessions: -1,
-  baseUrl: "http://127.0.0.1:31574",
-  rootElement: "body",
-  allScriptsTimeout: 11000,
-  getPageTimeout: 10000,
-  params: {
-    login: {
-      user: "Jane",
-      password: "1234"
+    multiCapabilities: [],
+    maxSessions: -1,
+    baseUrl: "http://127.0.0.1:31574",
+    rootElement: "body",
+    allScriptsTimeout: 11000,
+    getPageTimeout: 10000,
+    params: {
+      login: {
+        user: "Jane",
+        password: "1234"
+      }
+    },
+    framework: "jasmine",
+    jasmineNodeOpts: {
+      isVerbose: false,
+      showColors: true,
+      includeStackTrace: true,
+      defaultTimeoutInterval: 30000
+    },
+    mochaOpts: {
+      ui: "bdd",
+      reporter: "list"
     }
-  },
-  framework: "jasmine",
-  jasmineNodeOpts: {
-    isVerbose: false,
-    showColors: true,
-    includeStackTrace: true,
-    defaultTimeoutInterval: 30000
-  },
-  mochaOpts: {
-    ui: "bdd",
-    reporter: "list"
-  },
-  cucumberOpts: {
-    require: "cucumber/stepDefinitions.js",
-    tags: "@dev",
-    format: "summary"
-  }
+  };
 };
